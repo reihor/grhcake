16bb40a2f564c8114dffd916a5ba600b3140b4a4
horsttop <horsttop@163.com>
2015-07-27 22:03:40 +0800

    more smart for fetching annotations

--- JavaBasicSkills/src/com/cake/core/annotation/database/TableCreator.java
+++ JavaBasicSkills/src/com/cake/core/annotation/database/TableCreator.java
@@ -7,7 +7,12 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
-
+/**
+ * 本例被屏的取注解代码有个界限,即针对的field取的是第一个注解,没有考虑到一个field有多个注解的情况.
+ * 那些框架例如spring框架是怎么做的?
+ * @author horsttop
+ *
+ */
 public class TableCreator {
 	public static void main(String[] args) {
 		/*if (args.length<1) {
@@ -35,16 +40,19 @@ public class TableCreator {
 					Annotation[] anns = field.getDeclaredAnnotations();
 					if(anns.length<1)
 						continue;// 表里没有字段
-					//整型字段
+					
+					/*//整型字段
 					if(anns[0] instanceof SQLInteger){
 						SQLInteger sInt = (SQLInteger) anns[0];
+						//name是否赋值判断
 						if(sInt.name().length()<1)
 							columnName = field.getName().toUpperCase();
 						else
 							columnName = sInt.name();
 						columnDefs.add(columnName+" INT"+getConstraints(sInt.constraints()));
-					}
-					//字符型字段
+					}*/
+					
+					/*//字符型字段
 					if(anns[0] instanceof SQLString){
 						SQLString sString = (SQLString) anns[0];
 						if(sString.name().length()<1)
@@ -52,6 +60,27 @@ public class TableCreator {
 						else
 							columnName = sString.name();
 						columnDefs.add(columnName+" VARCHAR("+sString.value()+")"+getConstraints(sString.constraints()));
+					}*/
+					//解决一个field有多个注解
+					for(int i=0;i<anns.length;i++){
+						if(anns[i] instanceof SQLInteger){
+							SQLInteger sInt = (SQLInteger) anns[i];
+							//name是否赋值判断
+							if(sInt.name().length()<1)
+								columnName = field.getName().toUpperCase();
+							else
+								columnName = sInt.name();
+							columnDefs.add(columnName+" INT"+getConstraints(sInt.constraints()));
+						}
+						//字符型字段
+						if(anns[i] instanceof SQLString){
+							SQLString sString = (SQLString) anns[i];
+							if(sString.name().length()<1)
+								columnName = field.getName().toUpperCase();
+							else
+								columnName = sString.name();
+							columnDefs.add(columnName+" VARCHAR("+sString.value()+")"+getConstraints(sString.constraints()));
+						}
 					}
 					StringBuilder createCommand = new StringBuilder("CREATE TABLE"+tableName+"(");
 					for(String columnDef:columnDefs)
